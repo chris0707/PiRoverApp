f782d31cc8157e519c3ec0f5ed3cdda2307708bf
chris0707 <31682624+chris0707@users.noreply.github.com>
2017-10-11 14:35:29 -0400

    added test int

--- app/src/main/java/com/example/chris/piroverapp/LoginActivity.java
+++ app/src/main/java/com/example/chris/piroverapp/LoginActivity.java
@@ -10,6 +10,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        int test123;
+
 
     }
 }
