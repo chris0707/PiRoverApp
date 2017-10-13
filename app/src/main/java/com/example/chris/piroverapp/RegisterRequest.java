5c45dae624fbc71b76d8015382f13bb38ef2fe31
chris0707 <31682624+chris0707@users.noreply.github.com>
2017-10-12 23:38:31 -0400

    Updated register and login acitivities and their layouts

--- app/src/main/java/com/example/chris/piroverapp/RegisterRequest.java
+++ app/src/main/java/com/example/chris/piroverapp/RegisterRequest.java
@@ -1,8 +1,30 @@
 package com.example.chris.piroverapp;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Chris on 2017-10-11.
  */
 
-public class RegisterRequest {
+public class RegisterRequest extends StringRequest {
+    private static final String REGISTER_REQUEST_URL = "https://barebarz.000webhostapp.com/RegisterPi.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String name, String username, String password, String content, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params =  new HashMap<>();
+        params.put("name", name);
+        params.put("username", username);
+        params.put("password", password);
+        params.put("content", content);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
 }
