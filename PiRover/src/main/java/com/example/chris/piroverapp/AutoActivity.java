f16907334a7afc1993e961e5fb1ecf9a70819e05
n01033296 <31857617+n01033296@users.noreply.github.com>
2017-12-06 16:56:08 -0500

    AutoActivity And Splash.XML

--- PiRover/src/main/java/com/example/chris/piroverapp/AutoActivity.java
+++ PiRover/src/main/java/com/example/chris/piroverapp/AutoActivity.java
@@ -1,17 +1,44 @@
 package com.example.chris.piroverapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+import static java.lang.System.exit;
+
 public class
 AutoActivity extends AppCompatActivity {
+    private android.os.Handler handler = new android.os.Handler();
+
+    BluetoothAdapter blueAdapter;
+    BluetoothSocket blueSocket;
+    BluetoothDevice blueDevice;
+
+    OutputStream outputStream;
+    InputStream inputStream;
+
 
     Button toggleButton;
     Boolean check = Boolean.FALSE;
+
+    String a;
+    String message="";
+    boolean connected;
+    boolean runThreadRunning = false;
+    boolean runThreadStop = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,4 +70,108 @@ AutoActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public static boolean isBluetoothAvailable() {
+        final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        return (bluetoothAdapter != null && bluetoothAdapter.isEnabled());
+    }
+
+    void findBT() throws IOException {
+
+        blueAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (blueAdapter == null) {
+            exit(0);
+
+        }
+
+        if (!blueAdapter.isEnabled()) {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+
+        }
+
+        Set<BluetoothDevice> pairedDevices = blueAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+
+            for (BluetoothDevice device : pairedDevices) {
+                if (device.getName().equals(a)) {
+
+                    blueDevice = device;
+                    break;
+                }
+
+            }
+
+        }
+
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+        blueSocket = blueDevice.createRfcommSocketToServiceRecord(uuid);
+        try {
+            blueSocket.connect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            exit(0);
+        } finally {
+            outputStream = blueSocket.getOutputStream();
+            inputStream = blueSocket.getInputStream();
+        }
+
+    }
+
+    void sendMsg(String msg1) {
+        try {
+            outputStream.write(msg1.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+            //exit(0);
+        }
+    }
+
+    private void handleRunDown(String mes) {
+        if (!runThreadRunning)
+            startRunThread(mes);
+    }
+
+    private void startRunThread(final String mes) {
+        Thread r = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    runThreadRunning = true;
+                    while (!runThreadStop) {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                sendMsg(mes);
+
+                                //keepSending(message);
+                            }
+                        });
+                        try {
+                            Thread.sleep(50);
+
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException("Could not wait...", e);
+                        }
+
+                    }
+                } finally {
+                    runThreadRunning = false;
+                    runThreadStop = false;
+                }
+            }
+        };
+
+        r.start();
+    }
+
+    private void handleRunUp(){
+        runThreadStop = true;
+    }
+
 }
